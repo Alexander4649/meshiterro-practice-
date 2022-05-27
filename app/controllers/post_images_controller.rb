d37cd7bfb031689ce2a1171fd76460c198301434
Alexander4649 <105344687+Alexander4649@users.noreply.github.com>
2022-05-27 02:12:54 +0000

    [Start-???]meshiterro13章削除機能実装確認はできない

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -22,9 +22,11 @@ class PostImagesController < ApplicationController
   end
 
   def destroy
+    @post_image = PostImage.find(params[:id])
+    @post_image.destroy
+    redirect_to "/post_images"
   end
 
-  
   
   private # 投稿データのストロングパラメータ
 
