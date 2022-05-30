a58d19632e7a642cc15b1dbadc12f4b22e1ce8eb
Alexander4649 <105344687+Alexander4649@users.noreply.github.com>
2022-05-30 11:18:46 +0000

    [Start-???]userコントローラが多分間違っている

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id]) # URLに記載されたIDを参考に、必要なUserモデルを取得する
+    @post_images = @user.post_images  # 特定のユーザ(@user)に関連付けされた投稿全て(.post_images)を取得し、@post_imagesに渡す　という処理、個人が投稿したもの全てを表示できるようになる
   end
 
   def edit
