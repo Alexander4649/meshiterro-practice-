a767c1d86748c38b7516d267e032eb913ac53ba2
Alexander4649 <105344687+Alexander4649@users.noreply.github.com>
2022-05-31 05:32:22 +0000

    [Start-Finish]13章-railsでは全角スペースは禁止だから注意！

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id]) # URLに記載されたIDを参考に、必要なUserモデルを取得する
-    @post_images = @user.post_images  # 特定のユーザ(@user)に関連付けされた投稿全て(.post_images)を取得し、@post_imagesに渡す　という処理、個人が投稿したもの全てを表示できるようになる
+  def show#マイページアクション
+    @user = User.find(params[:id])# URLに記載されたIDを参考に、必要なUserモデルを取得する
+    @post_images = @user.post_images# 特定のユーザ(@user)に関連付けされた投稿全て(.post_images)を取得し、@post_imagesに渡す　という処理、個人が投稿したもの全てを表示できるようになる
   end
 
-  def edit
+  def edit#マイページの中の編集機能アクション
+    @user = User.find(params[:id])#特定のidを持ったレコードを取得する
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,8 @@ class User < ApplicationRecord
          has_one_attached :profile_image # profile_imageという名前でActiveStorageでプロフィール画像を保存できるように設定
 
   
- def get_profile_image(width,height)　#  プロフィール画像が存在しない場合に表示する画像をActiveStorageに格納する (引数の)
-    unless profile_image.attached? # ❶プロフィール画像がない時は
+ def get_profile_image(width,height)#  プロフィール画像が存在しない場合に表示する画像をActiveStorageに格納する (引数の)
+    unless profile_image.attached?# ❶プロフィール画像がない時は
       file_path = Rails.root.join('app/assets/images/sample-author1.jpg') # ❷ app/assets/imagesに格納されている nsample-author1.jpgという画像を
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg') # ❸デフォルト画像としてActiveStorageに格納し、格納した画像を表示する
     end
