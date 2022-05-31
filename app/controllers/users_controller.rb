0c203dde31e67817a7aa1728ec06a966e6569b17
Alexander4649 <105344687+Alexander4649@users.noreply.github.com>
2022-05-31 06:24:24 +0000

    [Finish]16章ー14章じゃなくて16だった。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,14 @@ class UsersController < ApplicationController
     @post_images = @user.post_images# 特定のユーザ(@user)に関連付けされた投稿全て(.post_images)を取得し、@post_imagesに渡す　という処理、個人が投稿したもの全てを表示できるようになる
   end
 
-  def edit#マイページの中の編集機能アクション
-    @user = User.find(params[:id])#特定のidを持ったレコードを取得する
+  def edit #マイページの中の編集機能アクション
+    @user = User.find(params[:id]) #特定のidを持ったレコードを取得する
   end
   
   def update
-    @user = User.find(params[:id])#ユーザーの取得
-    @user.update(user_params)#ユーザーのアップデート
-    redirect_to user_path#ユーザーの詳細ページへのパス  
+    @user = User.find(params[:id]) #ユーザーの取得
+    @user.update(user_params) #ユーザーのアップデート
+    redirect_to user_path #ユーザーの詳細ページへのパス  
   end
 
 
